ceb2bb13f7e14818933440943a5ea40ae12ff950
Ashray Shetty <22a21.ashray@sjec.ac.in>
2025-10-05 16:34:24 +0530

    final commit

--- app.js
+++ app.js
@@ -1,84 +1,100 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const Listing = require('./models/listing.js');
-const path = require('path');
-const methodOverride = require('method-override');
-const ejsMate=require('ejs-mate');
-
-const MongoURL='mongodb://localhost:27017/delta';
-
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'views'));
-app.use(express.urlencoded({extended:true}));
-app.use(methodOverride('_method'));
-app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname, '/public')));
-
-main().then(() => console.log('Connected to MongoDB'))
-.catch(err => console.log(err));
-
-async function main() {
-    await mongoose.connect(MongoURL);
+if(process.env.NODE_ENV !="production") {
+require("dotenv").config();
 }
 
-app.get('/', (req, res) => {
-    res.send('Hi i am root');
-});
 
-//new route
-app.get('/listings/new',(req,res)=>{
-    res.render('listings/new.ejs');
-});
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const ExpressError = require("./utils/ExpressError.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const session = require("express-session");
+const MongoStore = require('connect-mongo');
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const UserRouter   = require("./routes/user.js");
+const User = require("./models/user.js");
 
-//create route
-app.post('/listings',async (req,res)=>{
-    const newListing=req.body.listing;
-    await newListing.save();
-    res.redirect("/listings");
-    
-});
+const dbUrl = process.env.ATLASDB_URL;
 
-
-//show route
-app.get('/listings/:id',async (req,res)=>{
-    const {id} = req.params;
-    const listing = await Listing.findById(id);
-    if (!listing) {
-        return res.status(404).send('Listing not found');
-    }
-    else{res.render('listings/show.ejs',{listing});}
-    
-});
-
-//Index route
-app.get('/listings',async (req,res)=>{
-    const allListings = await Listing.find({});
-    res.render('listings/index.ejs',{allListings});
-});
-
-//Edit Route
-app.get("/listings/:id/edit", async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", { listing });
-  });
-  
-  //Update Route
-  app.put("/listings/:id", async (req, res) => {
-    let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-    res.redirect(`/listings/${id}`);
-  });
-  
-  //Delete Route
-  app.delete("/listings/:id", async (req, res) => {
-    let { id } = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
+main()
+  .then((res) => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
   });
 
-app.listen(8080, () => {
-    console.log('Server is running on port 8080 http://localhost:8080');
+async function main() {
+  await mongoose.connect(dbUrl);
+}
+
+// Middleware
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "/public")));
+
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  crypto: {
+    secret: process.env.SECRET,
+  },
+  touchAfter: 24 *3600,
+});
+store.on("error",  ()  =>{
+  console.log("ERROR in MONGO SESSION STORE", err);
+});
+
+
+const sessionOptions = {
+  store,
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000, // 7 days
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    httpOnly: true,
+  },
+};
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Make flash messages available in all views
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user;
+  next();
+});
+
+
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", UserRouter);
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message = "Something went wrong" } = err;
+  res.status(statusCode).render("error.ejs", { message, statusCode });
+});
+
+// Start Server
+app.listen(5050, () => {
+  console.log("Server is running on port 5050");
 });
